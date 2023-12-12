a8951b70340d3383483f9a9725bda9afb868ca2f
dmiv <dmitriy40647274@gmail.com>
2023-12-12 20:36:12 +0300

    universal mode in fetch useFetch page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import {resolve} from "path";
+// import {resolve} from "path";
 
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  alias: {
-    "@": resolve(__dirname, "/")
+  nitro: {
+    prerender: {
+      routes: [
+          "/fetch/useLazy"
+      ]
+    }
   },
+  ssr: true,
+  // alias: {
+  //   "@": resolve(__dirname, "/")
+  // },
   css: ["~/assets/main.scss"],
   postcss: {
     plugins: {
--- pages/fetch/useLazy/index.vue
+++ pages/fetch/useLazy/index.vue
@@ -1,11 +1,26 @@
 <script setup>
+import Loading from "~/components/Icons/loading.vue";
+
 definePageMeta({
   layout: "fetch"
 })
+
+const {data: products, pending } = useLazyFetch('/api/products')
+
+const {data: ssrProducts } = await useFetch(('/api/products'))
+
 </script>
 
 <template>
 use lazy
+  {{products}} {{pending}}
+  <icons-loading />
+  <div>
+    useFetch
+    <div>
+      {{ssrProducts}}
+    </div>
+  </div>
 </template>
 
 <style scoped>
